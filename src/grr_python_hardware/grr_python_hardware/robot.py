a038b0d8930c3ba9be1d69839db007fe79d4dce6
Philip Smith <philip@randomsmiths.com>
2024-01-08 19:11:18 -0500

    moving roboclaw library to the right place pt1

--- src/grr_python_hardware/grr_python_hardware/robot.py
+++ src/grr_python_hardware/grr_python_hardware/robot.py
@@ -2,9 +2,20 @@ from roboclaw_python_library.roboclaw_python.roboclaw_3 import Roboclaw
 import rclpy
 from rclpy.node import Node
 
-
 class Robot(Node):
     def __init__(self):
         super().__init__("Robot")
-        
+        self.roboclaw = Roboclaw("/dev/ttyS0", 38400)
+        self.roboclaw.Open()
 
+        
+def main():
+    rclpy.init()
+    robot = Robot()
+    rclpy.spin(robot)
+    
+    robot.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- src/grr_python_hardware/setup.py
+++ src/grr_python_hardware/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'robot = grr_python_hardware.robot:main',
         ],
     },
 )
