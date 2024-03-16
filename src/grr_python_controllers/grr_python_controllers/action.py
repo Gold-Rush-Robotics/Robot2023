85f6294cab2028940874000e809344e731f00392
philip <philip@randomsmiths.com>
2024-03-16 02:32:45 -0400

    clean up action class

--- src/grr_python_controllers/grr_python_controllers/action.py
+++ src/grr_python_controllers/grr_python_controllers/action.py
@@ -3,6 +3,10 @@ import rclpy
 from rclpy.node import Node
 import time
 
+from __future__ import annotations
+
+
+
 #msg imports
 from sensor_msgs.msg import Range, JointStates
 from nav_msgs.msg import Odometry
@@ -11,23 +15,25 @@ from trajectory_msgs.msg import JointTrajectory
 from std_msgs.msg import Bool, Float32
 
 
-class action:
-    nextAction : action
+class Action:
+    nextAction : Action
+    name: str
 
     def __init__(self) -> None:
-        pass
+        name = self.__qualname__
+        nextAction = Action()
 
-    def setNext(self, nextAction: action):
+    def setNext(self, nextAction: Action):
         nextAction.nextAction = self.nextAction
         self.nextAction = nextAction
         return self.nextAction
     
-    def setLast(self, lastAction: action):
+    def setLast(self, lastAction: Action):
         if(self.nextAction):
             return self.nextAction.setLast(lastAction)
         else:
             self.nextAction = lastAction
             return self.nextAction
         
-    def run(self):
-        return self.nextAction
+    def run(self, node:Node):
+        return self
